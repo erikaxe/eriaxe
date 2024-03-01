c1e20ceb161ee7700bc3b7dc7da2f7aad971c52a
erikaxe <69839306+erikaxe@users.noreply.github.com>
2024-03-01 17:11:36 +0100

    Added new prop content + minor styling

--- app/_components/Cards/WorkCard.js
+++ app/_components/Cards/WorkCard.js
@@ -2,36 +2,49 @@
 import React, { useState } from "react";
 import {
     Box,
-    CardContent,
-    CardMedia,
     Typography,
     Divider,
     Button,
+    Fade,
+    IconButton,
+    Collapse,
 } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import ProjectGrid from "@/app/_components/Grids/ProjectGrid";
 
-const WorkCard = () => {
+const WorkCard = ({ title, years, subHeader, desc }) => {
     const [open, setOpen] = useState(false);
 
     return (
         <Box
             sx={{
                 position: "relative",
-                color: "red",
+                color: "#000",
                 width: "100%",
-                height: "200px",
                 backgroundColor: "#fff",
                 borderRadius: "15px",
                 p: 3,
             }}>
-            <Typography variant="h3" className="fs-24">
-                Title
+            <Typography variant="h3" className="fs-24 fw-600">
+                {title}
             </Typography>
-            <Typography>years</Typography>
+            <Typography variant="body1" className="fs-16" mb={1} mt={0.3}>
+                {years}
+            </Typography>
             <Divider />
-            <Typography>description section</Typography>
+            <Typography variant="h4" className="fs-16 fw-600" my={1}>
+                {subHeader}
+            </Typography>
+            <Typography variant="body1" className="fs-16" mb={3}>
+                {desc}
+            </Typography>
+            <Collapse in={open} timeout="auto" /* unmountOnExit */>
+                <ProjectGrid />
+            </Collapse>
             <Button
                 variant="contained"
-                sx={{ position: "absolute", bottom: "24px" }}>
+                sx={{ mt: 3 }}
+                onClick={() => setOpen(!open)}>
                 View projects
             </Button>
         </Box>

46ec9669a446dab18c080911a8998bbb141f1110
erikaxe <69839306+erikaxe@users.noreply.github.com>
2024-03-03 17:58:41 +0100

    Create ContactButton.js

---
+++ app/_components/Buttons/ContactButton.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Box } from "@mui/material";
+import Link from "next/link";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+
+const iconComponents = {
+    linkedin: {
+        Icon: LinkedInIcon,
+        link: "https://www.linkedin.com/in/eriaxe",
+    },
+    mailto: {
+        Icon: EmailOutlinedIcon,
+        link: "mailto:your-email@example.com",
+    },
+};
+
+const ContactButton = ({ type }) => {
+    // Destructure the Icon and link from the iconComponents object based on the given type prop
+    const { Icon, link } = iconComponents[type] || {};
+
+    return (
+        Icon &&
+        link && (
+            <Link href={link} target="_blank">
+                <Box p={8} className="flex center border-white box-hover-white">
+                    <Icon style={{ fontSize: "100px" }} />
+                </Box>
+            </Link>
+        )
+    );
+};
+
+export default ContactButton;

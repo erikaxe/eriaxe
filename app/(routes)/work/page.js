72924f1e225d0870132c6b4d2cd44ba7418e65aa
erikaxe <69839306+erikaxe@users.noreply.github.com>
2024-03-03 17:58:37 +0100

    Update page.js

--- app/(routes)/work/page.js
+++ app/(routes)/work/page.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Box, Container } from "@mui/material";
-import CardGrid from "@/app/_components/Grids/CardGrid";
+import WorkGrid from "@/app/_components/Grids/WorkGrid";
 
 const page = () => {
     return (
         <Container maxWidth={"md"} sx={{ mt: "calc(30vh - 1.5em)" }}>
-            <Box>
-                <CardGrid />
-            </Box>
+            <WorkGrid skipLastIndex />
         </Container>
     );
 };

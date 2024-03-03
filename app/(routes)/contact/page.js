e15add35509b9aa543eea25d4353d33820e9eaf7
erikaxe <69839306+erikaxe@users.noreply.github.com>
2024-03-03 17:58:11 +0100

    Built the /contact page

--- app/(routes)/contact/page.js
+++ app/(routes)/contact/page.js
@@ -1,7 +1,19 @@
-import React from "react";
+import { Container, Box, Grid } from "@mui/material";
+import ContactButton from "@/app/_components/Buttons/ContactButton";
 
 const page = () => {
-    return <div>contact page</div>;
+    return (
+        <Container maxWidth={"lg"} sx={{ mt: "calc(30vh - 1.5em)" }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                    <ContactButton type={"linkedin"} />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <ContactButton type={"mailto"} />
+                </Grid>
+            </Grid>
+        </Container>
+    );
 };
 
 export default page;

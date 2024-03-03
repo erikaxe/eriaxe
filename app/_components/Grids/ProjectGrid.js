555c5807073864fd05232be1c798b1c144c2fc7b
erikaxe <69839306+erikaxe@users.noreply.github.com>
2024-03-03 18:00:15 +0100

    Update ProjectGrid.js

--- app/_components/Grids/ProjectGrid.js
+++ app/_components/Grids/ProjectGrid.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { Grid, Box } from "@mui/material";
+import { Grid } from "@mui/material";
 import projects from "@/public/projects.json";
 import ProjectCard from "@/app/_components/Cards/ProjectCard";
 
@@ -9,7 +8,13 @@ const ProjectGrid = () => {
             {projects.map((item, i) => {
                 return (
                     <Grid item xs={12} sm={6} md={4} key={i}>
-                        <ProjectCard title={item.title} url={item.url} />
+                        <ProjectCard
+                            title={item.title}
+                            url={item.url}
+                            img={item.img}
+                            alt={item.alt}
+                            color={item.color}
+                        />
                     </Grid>
                 );
             })}

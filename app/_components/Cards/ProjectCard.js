b4d4ed284977e2a59b8046b381ebebc5e25591c3
erikaxe <69839306+erikaxe@users.noreply.github.com>
2024-03-03 17:58:47 +0100

    Update ProjectCard.js

--- app/_components/Cards/ProjectCard.js
+++ app/_components/Cards/ProjectCard.js
@@ -5,32 +5,31 @@ import Link from "next/link";
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 import LanguageIcon from "@mui/icons-material/Language";
 
-const ProjectCard = ({ title, url }) => {
+const ProjectCard = ({ title, url, img, alt, color }) => {
     const [isHovered, setIsHovered] = useState(false);
 
     return (
         <>
-            <Box
-                className="flex"
-                position="relative"
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}>
-                <Image
-                    className="radius-8"
-                    src="/avie_og_img.jpg"
-                    alt="essential-long-sleeve-light-grey-med-silver-logo-ridtopp"
-                    width={0}
-                    height={0}
-                    sizes="100vw"
-                    style={{ width: "100%", height: "auto" }}
-                />
+            <Link href={url} target="_blank">
                 <Box
-                    className="position-bottom-right white"
-                    sx={{ display: { xs: "flex", sm: "none" } }}>
-                    <LanguageIcon />
-                </Box>
-                <Fade in={isHovered}>
-                    <Link href={url} target="_blank">
+                    className="flex max-height-171"
+                    position="relative"
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}>
+                    <Image
+                        className="radius-8 full-width"
+                        src={img}
+                        alt={alt}
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                    />
+                    <Box
+                        className="position-bottom-right white"
+                        sx={{ display: { xs: "flex", sm: "none" } }}>
+                        <LanguageIcon sx={{ color: color }} />
+                    </Box>
+                    <Fade in={isHovered}>
                         <Box
                             className="radius-8 flex center"
                             sx={{
@@ -41,11 +40,14 @@ const ProjectCard = ({ title, url }) => {
                                 height: "100%",
                                 backgroundColor: "rgba(255, 255, 255, 0.5)",
                             }}>
-                            <LanguageIcon className="fs-60" />
+                            <LanguageIcon
+                                className="fs-60"
+                                sx={{ color: color }}
+                            />
                         </Box>
-                    </Link>
-                </Fade>
-            </Box>
+                    </Fade>
+                </Box>
+            </Link>
             <Typography variant="h3" className="fs-16" mt={1}>
                 {title}
             </Typography>

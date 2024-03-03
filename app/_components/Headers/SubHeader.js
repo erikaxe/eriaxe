73771432faf674ddcacf965060fffe1eebb3aac4
erikaxe <69839306+erikaxe@users.noreply.github.com>
2024-03-03 18:00:32 +0100

    Create SubHeader.js

---
+++ app/_components/Headers/SubHeader.js
@@ -0,0 +1,19 @@
+import { Typography, Divider, Box } from "@mui/material";
+
+const SubHeader = ({ text }) => {
+    return (
+        <Box my={5}>
+            <Typography variant="h2" className="fs-18 fw-600 ls-2">
+                {text}
+            </Typography>
+            <Divider
+                sx={{
+                    my: 3,
+                    borderColor: "#4d4b4b",
+                }}
+            />
+        </Box>
+    );
+};
+
+export default SubHeader;

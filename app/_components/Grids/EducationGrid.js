51ce2a718cffd9949820d900a7c5937241335bb0
erikaxe <69839306+erikaxe@users.noreply.github.com>
2024-03-03 18:00:10 +0100

    Create EducationGrid.js

---
+++ app/_components/Grids/EducationGrid.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Grid, Typography } from "@mui/material";
+import education from "@/public/education.json";
+import BasicList from "@/app/_components/Lists/BasicList";
+
+const EducationGrid = () => {
+    return (
+        <Grid container spacing={2}>
+            {education.map((item, i) => {
+                return (
+                    <React.Fragment key={i}>
+                        <Grid item xs={12} sm={8}>
+                            <Typography variant="h3" className="fs-20 fw-600">
+                                {item.title}
+                            </Typography>
+                            <Typography variant="body1" className="fs-15">
+                                {item.subHeader}
+                            </Typography>
+                            <Typography variant="body1" className="fs-15">
+                                {item.years}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <BasicList
+                                data={item.courses}
+                                title={item.listTitle}
+                            />
+                        </Grid>
+                    </React.Fragment>
+                );
+            })}
+        </Grid>
+    );
+};
+
+export default EducationGrid;

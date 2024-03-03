af6110155a20d7a0445aa9731bef308748437f00
erikaxe <69839306+erikaxe@users.noreply.github.com>
2024-03-03 18:00:29 +0100

    Create WorkGrid.js

---
+++ app/_components/Grids/WorkGrid.js
@@ -0,0 +1,37 @@
+import workExperience from "@/public/workExperience.json";
+import { Grid } from "@mui/material";
+import WorkCard from "@/app/_components/Cards/WorkCard";
+
+const WorkGrid = ({ skipLastIndex }) => {
+    return (
+        <Grid container spacing={2}>
+            {workExperience.map((item, i) => {
+                let smValue;
+
+                if (i % 3 === 0) {
+                    smValue = 12; // Every third item gets a value of 12
+                } else {
+                    smValue = 6; // Other items get a value of 6
+                }
+
+                // Check if it's the last index and skip if needed
+                if (skipLastIndex && i === workExperience.length - 1) {
+                    return null;
+                }
+
+                return (
+                    <Grid item xs={12} sm={smValue} key={i}>
+                        <WorkCard
+                            title={item.title}
+                            years={item.years}
+                            subHeader={item.subHeader}
+                            desc={item.desc}
+                        />
+                    </Grid>
+                );
+            })}
+        </Grid>
+    );
+};
+
+export default WorkGrid;

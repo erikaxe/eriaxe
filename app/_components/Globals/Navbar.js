9e447da6ab556332bfd37ea7ce878c5d6eecf183
erikaxe <69839306+erikaxe@users.noreply.github.com>
2024-03-04 15:16:30 +0100

    Removed unused code & imports

--- app/_components/Globals/Navbar.js
+++ app/_components/Globals/Navbar.js
@@ -1,57 +1,9 @@
-import * as React from "react";
-import {
-    Box,
-    AppBar,
-    Toolbar,
-    Container,
-    useScrollTrigger,
-    Fab,
-    Fade,
-    Divider,
-    Grid,
-} from "@mui/material";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import { Box, Container, Grid } from "@mui/material";
 import Link from "next/link";
-import Image from "next/image";
 import NavLink from "@/app/_components/Links/NavLink";
 import MenuDrawer from "@/app/_components/Drawers/MenuDrawer";
 
-function ScrollTop(props) {
-    const { children, window } = props;
-    // Note that you normally won't need to set the window ref as useScrollTrigger
-    // will default to window.
-    // This is only being set here because the demo is in an iframe.
-    const trigger = useScrollTrigger({
-        target: window ? window() : undefined,
-        disableHysteresis: true,
-        threshold: 100,
-    });
-
-    const handleClick = (event) => {
-        const anchor = (event.target.ownerDocument || document).querySelector(
-            "#back-to-top-anchor"
-        );
-
-        if (anchor) {
-            anchor.scrollIntoView({
-                block: "center",
-            });
-        }
-    };
-
-    return (
-        <Fade in={trigger}>
-            <Box
-                onClick={handleClick}
-                role="presentation"
-                sx={{ position: "fixed", bottom: 16, right: 16 }}>
-                {children}
-            </Box>
-        </Fade>
-    );
-}
-
-export default function BackToTop(props) {
+const Navbar = () => {
     return (
         <nav>
             <Box
@@ -98,13 +50,8 @@ export default function BackToTop(props) {
                     </Grid>
                 </Container>
             </Box>
-            {/* <Toolbar id="back-to-top-anchor" />
-
-            <ScrollTop {...props}>
-                <Fab size="small" aria-label="scroll back to top">
-                    <KeyboardArrowUpIcon />
-                </Fab>
-            </ScrollTop> */}
         </nav>
     );
-}
+};
+
+export default Navbar;

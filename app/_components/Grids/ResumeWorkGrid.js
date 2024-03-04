6ced32f462b1ce50fdab304d0bbeb53552b12479
erikaxe <69839306+erikaxe@users.noreply.github.com>
2024-03-04 15:16:40 +0100

    Removed unused import

--- app/_components/Grids/ResumeWorkGrid.js
+++ app/_components/Grids/ResumeWorkGrid.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Typography, Box } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import workExperience from "@/public/workExperience.json";
 
 const ResumeWorkGrid = () => {

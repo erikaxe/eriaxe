d4976b0ab9a1763797a9d7953dfabf0ece6ee38a
erikaxe <69839306+erikaxe@users.noreply.github.com>
2024-03-04 15:16:15 +0100

    Removed unused code & import

--- app/_components/Globals/Globals.js
+++ app/_components/Globals/Globals.js
@@ -1,17 +1,12 @@
 "use client";
 import Navbar from "@/app/_components/Globals/Navbar";
-/* import DrawerNavigation from "@/app/_components/Globals/DrawerNavigation"; */
-import { usePathname } from "next/navigation";
 
 const Globals = () => {
     // Exclude the following pathnames from rendering the TopBar and DrawerNavigation components
-    const currentPathname = usePathname();
-    const disablePathname = ["/signin"];
 
     return (
         <div style={{ marginBottom: "109px" }}>
             <Navbar />
-            {/* <DrawerNavigation /> */}
         </div>
     );
 };

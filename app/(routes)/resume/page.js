3b5f5699c5a398d61d6920042ca7a05ca07d80e5
erikaxe <69839306+erikaxe@users.noreply.github.com>
2024-03-07 15:33:32 +0100

    Added <section> for each Grid component

--- app/(routes)/resume/page.js
+++ app/(routes)/resume/page.js
@@ -7,20 +7,28 @@ import ResumeWorkGrid from "@/app/_components/Grids/ResumeWorkGrid";
 const page = () => {
     return (
         <Container maxWidth="lg" sx={{ mt: "calc(30vh - 1.5em)" }}>
-            <Typography variant="h1" className="fs45-30 fw-600">
-                Erik Axelsson
-            </Typography>
-            <Typography variant="body1" className="fs21-15 lh-34" my={4}>
-                En noggrann front-end-utvecklare med en passion för att omvandla
-                design och wireframes till responsiva och användarvänliga
-                webbplatser.
-            </Typography>
-            <SubHeader text={"Färdigheter"} />
-            <SkillsGrid />
-            <SubHeader text={"Utbildning"} />
-            <EducationGrid />
-            <SubHeader text={"Arbete"} />
-            <ResumeWorkGrid />
+            <section>
+                <Typography variant="h1" className="fs45-30 fw-600">
+                    Erik Axelsson
+                </Typography>
+                <Typography variant="body1" className="fs21-15 lh-34" my={4}>
+                    En noggrann front-end-utvecklare med en passion för att
+                    omvandla design och wireframes till responsiva och
+                    användarvänliga webbplatser.
+                </Typography>
+            </section>
+            <section>
+                <SubHeader text={"Färdigheter"} />
+                <SkillsGrid />
+            </section>
+            <section>
+                <SubHeader text={"Utbildning"} />
+                <EducationGrid />
+            </section>
+            <section>
+                <SubHeader text={"Arbete"} />
+                <ResumeWorkGrid />
+            </section>
         </Container>
     );
 };

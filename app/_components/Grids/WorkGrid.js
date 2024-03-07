d512b4418d0c2f3e0e469f4cefde2b124b62d0dc
erikaxe <69839306+erikaxe@users.noreply.github.com>
2024-03-07 15:35:44 +0100

    Wrapped component in <section>

--- app/_components/Grids/WorkGrid.js
+++ app/_components/Grids/WorkGrid.js
@@ -4,33 +4,37 @@ import WorkCard from "@/app/_components/Cards/WorkCard";
 
 const WorkGrid = ({ skipLastIndex }) => {
     return (
-        <Grid container spacing={2}>
-            {workExperience.map((item, i) => {
-                let smValue;
+        <section>
+            <Grid container spacing={2}>
+                {workExperience.map((item, i) => {
+                    let smValue;
 
-                if (i % 3 === 0) {
-                    smValue = 12; // Every third item gets a value of 12
-                } else {
-                    smValue = 6; // Other items get a value of 6
-                }
+                    if (i % 3 === 0) {
+                        smValue = 12; // Every third item gets a value of 12
+                    } else {
+                        smValue = 6; // Other items get a value of 6
+                    }
 
-                // Check if it's the last index and skip if needed
-                if (skipLastIndex && i === workExperience.length - 1) {
-                    return null;
-                }
+                    // Check if it's the last index and skip if needed
+                    if (skipLastIndex && i === workExperience.length - 1) {
+                        return null;
+                    }
 
-                return (
-                    <Grid item xs={12} sm={smValue} key={i}>
-                        <WorkCard
-                            title={item.title}
-                            years={item.years}
-                            subHeader={item.subHeader}
-                            desc={item.desc}
-                        />
-                    </Grid>
-                );
-            })}
-        </Grid>
+                    return (
+                        <Grid item xs={12} sm={smValue} key={i}>
+                            <article>
+                                <WorkCard
+                                    title={item.title}
+                                    years={item.years}
+                                    subHeader={item.subHeader}
+                                    desc={item.desc}
+                                />
+                            </article>
+                        </Grid>
+                    );
+                })}
+            </Grid>
+        </section>
     );
 };
 

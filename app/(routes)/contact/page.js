56a75acdea34504e59352e509eb645c3401cfe3b
erikaxe <69839306+erikaxe@users.noreply.github.com>
2024-03-26 10:49:20 +0100

    Wrap page content in section

--- app/(routes)/contact/page.js
+++ app/(routes)/contact/page.js
@@ -3,16 +3,18 @@ import ContactButton from "@/app/_components/Buttons/ContactButton";
 
 const page = () => {
     return (
-        <Container maxWidth={"lg"} sx={{ mt: "calc(30vh - 1.5em)" }}>
-            <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                    <ContactButton type={"linkedin"} />
+        <section>
+            <Container maxWidth={"lg"} sx={{ mt: "calc(30vh - 1.5em)" }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                        <ContactButton type={"linkedin"} />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <ContactButton type={"mailto"} />
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sm={6}>
-                    <ContactButton type={"mailto"} />
-                </Grid>
-            </Grid>
-        </Container>
+            </Container>
+        </section>
     );
 };
 

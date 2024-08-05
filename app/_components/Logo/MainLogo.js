ebce5093941b6a3894e654e4846b3b4d55bf1577
erikaxe <69839306+erikaxe@users.noreply.github.com>
2024-08-05 17:02:14 +0200

    Added new class, added explanatory aria-label, added svg title

--- app/_components/Logo/MainLogo.js
+++ app/_components/Logo/MainLogo.js
@@ -2,13 +2,17 @@ import Link from "next/link";
 
 const MainLogo = () => {
     return (
-        <Link href="/">
+        <Link
+            className="flex width-fit-content"
+            href="/"
+            aria-label="Image of logo that navigates to the Home page">
             <svg
                 width="60"
                 height="50"
                 viewBox="0 0 65 57"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg">
+                <title>Image of logo</title>
                 <path d="M32.5 0L64.976 56.25H0.0240479L32.5 0Z" fill="white" />
             </svg>
         </Link>

bd89290b563c779954f9df9b4955b62e6463ab76
erikaxe <69839306+erikaxe@users.noreply.github.com>
2024-08-07 15:14:47 +0200

    Import and use <ReportAccessibility /> component

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./styles/globals.scss";
 import Providers from "@/app/providers";
 import Globals from "@/app/_components/Globals/Globals";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import ReportAccessibility from "@/app/_utils/reportAccessibility";
 
 const roboto = Roboto({
     weight: ["100", "300", "400", "500", "700", "900"],
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
             <body className={`${roboto.className} m-b-70`}>
                 <Providers>
                     <Globals />
+                    <ReportAccessibility />
                     <main>{children}</main>
                     <SpeedInsights />
                 </Providers>

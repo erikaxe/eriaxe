37149d9f5cfcbf91916b09897235b35ee0806df9
erikaxe <69839306+erikaxe@users.noreply.github.com>
2024-09-06 11:27:47 +0200

    Added SR-only H1 element

--- app/(routes)/contact/page.js
+++ app/(routes)/contact/page.js
@@ -1,10 +1,13 @@
-import { Container, Grid } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
 import ContactButton from "@/app/_components/Buttons/ContactButton";
 
 const page = () => {
     return (
         <section>
             <Container maxWidth={"lg"} sx={{ mt: "calc(30vh - 1.5em)" }}>
+                <Typography variant="h1" className="sr-only">
+                    How to contact me
+                </Typography>
                 <Grid container spacing={2}>
                     <Grid item xs={12} sm={6}>
                         <ContactButton type={"linkedin"} />

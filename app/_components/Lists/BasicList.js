76417d14c83954c7d77444b4cf1eb576e77d623f
erikaxe <69839306+erikaxe@users.noreply.github.com>
2024-09-06 11:31:33 +0200

    Moved H3 element out from <List/> for better accessibility

--- app/_components/Lists/BasicList.js
+++ app/_components/Lists/BasicList.js
@@ -3,16 +3,19 @@ import { List, ListItem, Typography } from "@mui/material";
 const BasicList = ({ data, title }) => {
     return (
         data && (
-            <List className="lh-30">
+            <>
                 <Typography variant="h3" className="fs-16 fw-700">
                     {title}
                 </Typography>
-                {data.map((skill, i) => (
-                    <ListItem key={i} disablePadding className="fs-15">
-                        {skill}
-                    </ListItem>
-                ))}
-            </List>
+
+                <List className="lh-30">
+                    {data.map((skill, i) => (
+                        <ListItem key={i} disablePadding className="fs-15">
+                            {skill}
+                        </ListItem>
+                    ))}
+                </List>
+            </>
         )
     );
 };

e618a733c91a4d4d551553e3692f96429c23e93b
erikaxe <69839306+erikaxe@users.noreply.github.com>
2024-09-06 11:29:20 +0200

    Added aria-labels to Link element

--- app/_components/Buttons/ContactButton.js
+++ app/_components/Buttons/ContactButton.js
@@ -8,21 +8,23 @@ const iconComponents = {
     linkedin: {
         Icon: LinkedInIcon,
         link: "https://www.linkedin.com/in/eriaxe",
+        ariaLabel: "Visit Erik's LinkedIn profile",
     },
     mailto: {
         Icon: EmailOutlinedIcon,
         link: "mailto:erikio93@hotmail.com",
+        ariaLabel: "Send an email to Erik",
     },
 };
 
 const ContactButton = ({ type }) => {
     // Destructure the Icon and link from the iconComponents object based on the given type prop
-    const { Icon, link } = iconComponents[type] || {};
+    const { Icon, link, ariaLabel } = iconComponents[type] || {};
 
     return (
         Icon &&
         link && (
-            <Link href={link} target="_blank">
+            <Link href={link} target="_blank" aria-label={ariaLabel}>
                 <Box p={8} className="flex center border-white box-hover-white">
                     <Icon style={{ fontSize: "100px" }} />
                 </Box>
